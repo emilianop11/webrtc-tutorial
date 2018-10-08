b36b45e5ae4cdae5c94195d26f9bcb69a8191c16
Emiliano Pelliccioni <emilianop11@gmail.com>
2018-10-08 13:16:27 -0300

    Removes require for voxbone module leftover

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const Voxbone = require('voxbone-webrtc');
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
